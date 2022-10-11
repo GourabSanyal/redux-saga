0d62ecb8be7a2b81ae985b09526fcde1e96cc4bc
gourab <biz.gourab@gmail.com>
2022-10-11 13:08:08 +0530

    store set up

---
+++ src/redux/action.js
@@ -0,0 +1,8 @@
+export const addToCart = (data) => {
+  console.log("Action called", data);
+  return {
+    type: 'ADD_TO_CART',
+    data: data 
+  }
+  
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,6 @@ const dummyReducer = () => {
     return 100
 }
 
-const store = cretateStore(dummyReducer);
+const store = createStore(dummyReducer);
 
 export default store;
